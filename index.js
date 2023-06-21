5fdb4a67465e29444d46a6dde57e2971d6018dc4
Millz98 <dave_m86@hotmail.com>
2023-06-21 17:43:19 -0600

    added code to output scraping results

--- index.js
+++ index.js
@@ -1,32 +1,33 @@
+const fs = require('fs');
 const PORT = 4000;
-const axios = require("axios");
-const cheerio = require("cheerio");
-const express = require("express");
+const axios = require('axios');
+const cheerio = require('cheerio');
+const express = require('express');
 
 const app = express();
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
-app.get("/api/crypto", (req, res) => {
-  const url = "https://coinmarketcap.com/";
+app.get('/api/crypto', (req, res) => {
+  const url = 'https://coinmarketcap.com/';
   axios(url)
     .then((response) => {
       const html_data = response.data;
       const $ = cheerio.load(html_data);
 
-      const selectedElem = "tbody tr";
+      const selectedElem = 'tbody tr';
       const keys = [
-        "Symbol",
-        "Name",
-        "MarketCap",
-        "Price",
-        "Volume",
-        "Supply",
-        "Change",
-        "Change24h",
-        "Chart",
-        "Trade",
-        "TotalSupply",
-        "CirculatingSupply",
+        'Symbol',
+        'Name',
+        'MarketCap',
+        'Price',
+        'Volume',
+        'Supply',
+        'Change',
+        'Change24h',
+        'Chart',
+        'Trade',
+        'TotalSupply',
+        'CirculatingSupply',
       ];
 
       const coinArray = [];
@@ -35,7 +36,7 @@ app.get("/api/crypto", (req, res) => {
         const coinDetails = {};
 
         $(parentElem)
-          .find("td")
+          .find('td')
           .each((childId, childElem) => {
             const value = $(childElem).text().trim();
             coinDetails[keys[childId]] = value;
@@ -45,11 +46,23 @@ app.get("/api/crypto", (req, res) => {
       });
 
       console.log(coinArray);
-      res.send(coinArray);
+
+      // Convert the scraping results to JSON format
+      const jsonData = JSON.stringify(coinArray, null, 2);
+
+      // Write the JSON data to a file
+      fs.writeFile('scraped_data.json', jsonData, (err) => {
+        if (err) {
+          console.error(err);
+          res.status(500).send('Error occurred during scraping.');
+        } else {
+          console.log('Scraping results saved to scraped_data.json');
+          res.send(coinArray);
+        }
+      });
     })
     .catch((error) => {
       console.error(error);
-      res.status(500).send("Error occurred during scraping.");
+      res.status(500).send('Error occurred during scraping.');
     });
 });
-// Comment to test git push/pull
